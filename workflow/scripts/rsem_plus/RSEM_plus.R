d867f9ae45337520a0c1de01eb479c9ec6278fbd
isaacvock <isaac.vock@gmail.com>
2024-02-09 06:53:33 -0500

    Upped default EM iterations

--- workflow/scripts/rsem_plus/RSEM_plus.R
+++ workflow/scripts/rsem_plus/RSEM_plus.R
@@ -53,7 +53,7 @@ option_list <- list(
                     default = -1,
                     help = 'background mutation rate in old reads'),
     make_option(c("-i", "--niter", type="integer"),
-                default = 10,
+                default = 20,
                 help = 'Number of EM iterations'))
 
 opt_parser <- OptionParser(option_list = option_list)
