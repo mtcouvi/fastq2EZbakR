82796f84c381d23f45304622a49223eb3871680d
isaacvock <isaac.vock@gmail.com>
2024-03-30 13:47:53 -0400

    Fix data.table syntax

--- workflow/scripts/bam2bakR/merge_features_and_muts.R
+++ workflow/scripts/bam2bakR/merge_features_and_muts.R
@@ -204,7 +204,7 @@ cols_to_keep <- c("sample", feature_vect, muts_to_keep, bases_to_keep)
 
 muts[, sample := opt$sample]
 
-muts <- muts[, .(n = sum(n)), by = cols_to_keep]
+muts <- muts[, .(n = sum(n)), by = ..cols_to_keep]
 
 write_csv(muts,
           file = opt$cBoutput)
