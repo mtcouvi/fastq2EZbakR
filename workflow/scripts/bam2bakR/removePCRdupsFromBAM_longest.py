9b62973c62b84843a4a689236a3c378ef28db5f3
mtcouvi <62816886+mtcouvi@users.noreply.github.com>
2025-07-07 10:41:45 -0700

    Add files via upload

---
+++ workflow/scripts/bam2bakR/removePCRdupsFromBAM_longest.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+"""
+
+
+Author : chatGPT and M. Couvillion
+
+Remove reads in bam file resulting from PCR duplicates and keep the longest
+"""
+import sys, pysam
+
+input_bam = snakemake.input.input_bam
+output_bam = snakemake.output.output_bam
+
+iBAM = pysam.AlignmentFile(input_bam, 'rb')
+oBAM = pysam.AlignmentFile(output_bam, 'wb', template=iBAM)
+
+pysam.index(input_bam)
+
+current_chrom = None
+last_pos = None
+MB = dict()
+
+
+def flush_MB(MB, oBAM):
+    for _, (_, read) in MB.items():
+        oBAM.write(read)
+
+
+for read in iBAM.fetch(until_eof=True):
+    if read.is_unmapped:
+        continue
+
+    try:
+        mb = read.query_name.split('_MolecularBarcode:')[1]
+    except IndexError:
+        continue
+
+    chrom = iBAM.get_reference_name(read.reference_id)
+
+    # Flush cache when chromosome changes
+    if chrom != current_chrom:
+        flush_MB(MB, oBAM)
+        MB.clear()
+        current_chrom = chrom
+
+    # Strand + position
+    if not read.is_reverse:
+        start = read.reference_start
+        std = 'pos'
+    else:
+        start = read.reference_end
+        std = 'neg'
+
+    cigar = read.cigarstring
+    flag = read.flag
+    length = read.reference_length
+    
+    key = f"{chrom}_{std}_{mb}"
+    try:
+        value = read.get_tag("NM")
+    except KeyError:
+        continue
+
+#     if key not in MB or (value < MB[key][0] and length > MB[key][2]):
+#         MB[key] = (value, read, length)
+    if key not in MB or length > MB[key][0]:
+        MB[key] = (length,read)
+
+    # Optional: Flush reads if MB gets too big
+    if len(MB) > 1_000_000:
+        flush_MB(MB, oBAM)
+        MB.clear()
+
+# Final flush
+flush_MB(MB, oBAM)
+iBAM.close()
+oBAM.close()
+
+        
+# module load dev/python/2.7.10
+
