52f1d8f06c6e5ae912e428104b52426e7c1bccad
mtcouvi <62816886+mtcouvi@users.noreply.github.com>
2025-08-05 15:29:49 -0700

    Add files via upload

--- workflow/scripts/bam2bakR/merge_features_and_muts.R
+++ workflow/scripts/bam2bakR/merge_features_and_muts.R
@@ -404,7 +404,7 @@ write_csv(muts,
 muts_to_keep <- unlist(strsplit(opt$muttypes, ","))
 bases_to_keep <- paste0("n", substr(muts_to_keep, start = 1, stop = 1))
 
-cols_to_keep <- c("sample", "rname", feature_vect, muts_to_keep, bases_to_keep, "sj")
+cols_to_keep <- c("sample", "rname", feature_vect, muts_to_keep, bases_to_keep, "sj",  "cellbc") # cellbc added by MTC
 
 muts[, sample := opt$sample]
 
